d48a4da07cd51323a025b3647f50c8a99c248b75
antonmontrezor <montrezoranton991@gmail.com>
2021-08-09 22:22:23 -0400

    added create/edit functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,10 +9,35 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
+  end
 
+  def edit 
+    @article = Article.find(params[:id])
   end
 
   def create
-    render plain: params[:article]
+    # specifying keys we want to permit to be saved to an instance variable
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    # we can look at @article closer by specifying @article.inspect
+    if @article.save
+      # we give a notice a name we want, like :notice
+      flash[:notice] = "Article was created successfully!"
+      # redirect_to article_path(@article) is the same as below
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    # we are whitelisting a title and a description so they are available to use
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully."
+      redirect_to @article
+    else
+      render 'edit'
+    end 
   end
 end
