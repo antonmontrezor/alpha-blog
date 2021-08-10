f48b6dbc9aff8b82b493e40481e82394a5fa97eb
antonmontrezor <montrezoranton991@gmail.com>
2021-08-09 21:00:59 -0400

    added a form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index] # to keep only route for show action/view
+  resources :articles, only: [:show, :index, :new, :create] # to keep only route for the listed action
 end
