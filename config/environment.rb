3cc71553d9af1591a32a13829781e3bb0adc1de5
antonmontrezor <montrezoranton991@gmail.com>
2021-08-20 14:59:10 -0400

    added styles to the rest of pages/cleaned up code

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,8 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Adding these lines of code to disable rails from injecting a div with a class "fields_with_errors" that messes up layout of 'create article' page
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
