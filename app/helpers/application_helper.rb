21376ac64b129db2ba7ca84074fcd3456c0fd6d2
antonmontrezor <montrezoranton991@gmail.com>
2021-09-22 22:11:07 -0400

    add users show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+
+  def gravatar_for(user, options = { size: 80 })
+    # steps how to use gravatar -> https://en.gravatar.com/site/implement/images/ruby/
+    email_address = user.email.downcase
+    hash = Digest::MD5.hexdigest(email_address)
+    size = options[:size]
+    gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+    # we can return a tag to views
+    image_tag(gravatar_url, alt: user.username, class: 'rounded shadow mx-auto d-block')
+  end
 end
