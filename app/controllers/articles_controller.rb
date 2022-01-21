c6e76e309c852c48cced41b7744ca26997cb6db4
antonmontrezor <montrezoranton991@gmail.com>
2022-01-21 10:42:09 -0500

    enhance user/admin experience in the app

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,8 @@
 class ArticlesController < ApplicationController
   # we are adding a method :set_article as a symbol to a method before_action, so that set_article can be performed on all specified actions first -> no need to call set_article in all those actions separatelly
   before_action :set_article, only: %i[show edit update destroy]
+  before_action :require_user, except: %i[show index]
+  before_action :require_same_user, only: %i[edit update destroy]
 
   def show; end
 
@@ -49,6 +51,10 @@ class ArticlesController < ApplicationController
     redirect_to articles_path
   end
 
+  def hello
+    puts 'Hello'
+  end
+
   private
 
   # All methods defined below "private" will only be available within this controller
@@ -58,5 +64,12 @@ class ArticlesController < ApplicationController
 
   def article_params
     params.require(:article).permit(:title, :description)
+  end
+
+  def require_same_user
+    if current_user != @article.user && !current_user.admin?
+      flash[:alert] = 'You can only edit or delete your own article'
+      redirect_to @article
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@
 class User < ApplicationRecord
   # alter an object we are creating through console or users create on web before saving to database
   before_save { self.email = email.downcase }
-  has_many :articles
+  has_many :articles, dependent: :destroy # set up a dependant of a user class, so a dependant is deleted if a user is deleted
   validates :username, presence: true,
                        uniqueness: { case_sensitive: false },
                        length: { minimum: 3, maximum: 25 }
